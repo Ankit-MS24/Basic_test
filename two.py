8406a6dd6b2d9f4fd01e19ea6063b5622053ddbd
Ankit-MS24 <ankit@mobilesentrix.com>
2025-03-27 11:37:05 +0530

    Besic test

--- two.py
+++ two.py
@@ -1,12 +1,18 @@
 import pytest
 
+
 def add(x, y):
     return x + y
+
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+
 if __name__ == "__main__":
-    pytest.main()
+    pytest.main(["-v", "--junitxml=reports/test-results.xml"])
+
+
+
